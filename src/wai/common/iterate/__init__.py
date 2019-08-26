b9c261147a2aa9fc18c9d30984cb6caab57b729e
csterling <coreytsterling@gmail.com>
2019-08-27 11:40:45 +1200

    Added first to public set.

--- src/wai/common/iterate/__init__.py
+++ src/wai/common/iterate/__init__.py
@@ -1,4 +1,4 @@
 """
 Utility functions for working with iterables and iterators.
 """
-from ._functions import safe_iter, flatten, invert_indices, extract_by_index, is_iterable
+from ._functions import safe_iter, flatten, invert_indices, extract_by_index, is_iterable, first
