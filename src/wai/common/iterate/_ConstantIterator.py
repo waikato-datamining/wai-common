707ca311d3396cfd941448d1bce3e0ff2c7f1905
csterling <coreytsterling@gmail.com>
2019-08-28 16:54:14 +1200

    Modified ConstantIterator to use typing's T type variable.

--- src/wai/common/iterate/_ConstantIterator.py
+++ src/wai/common/iterate/_ConstantIterator.py
@@ -1,16 +1,18 @@
 from collections import Iterator
-from typing import _T_co, Optional
+from typing import Optional
+
+from .._typing import T
 
 
-class ConstantIterator(Iterator[_T_co]):
+class ConstantIterator(Iterator[T]):
     """
     Class which implements the iterator interface, but always returns a single value.
     """
-    def __init__(self, value: _T_co, num_iterations: Optional[int] = None):
-        self._value: _T_co = value
+    def __init__(self, value: T, num_iterations: Optional[int] = None):
+        self._value: T = value
         self._num_iterations: Optional[int] = num_iterations
 
-    def __next__(self) -> _T_co:
+    def __next__(self) -> T:
         if self._num_iterations == 0:
             raise StopIteration
         elif self._num_iterations is not None:
