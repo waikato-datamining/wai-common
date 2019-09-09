22cd3337d6f295ac2cb02e780b11421ba897286c
csterling <coreytsterling@gmail.com>
2019-09-09 14:45:11 +1200

    Added all_meet_predicate, any_meets_predicate to public set.

--- src/wai/common/iterate/__init__.py
+++ src/wai/common/iterate/__init__.py
@@ -2,5 +2,6 @@
 Utility functions for working with iterables and iterators.
 """
 from ._ConstantIterator import ConstantIterator
-from ._functions import safe_iter, flatten, invert_indices, extract_by_index, is_iterable, first, random
+from ._functions import safe_iter, flatten, invert_indices, extract_by_index, is_iterable, first, random, \
+    all_meet_predicate, any_meets_predicate
 from ._metadata import with_metadata, zip_metadata, get_metadata, unzip_metadata
