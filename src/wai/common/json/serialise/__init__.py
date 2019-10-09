bf9d9b1ff40b05bb7342891640b52cc95617aeea
csterling <coreytsterling@gmail.com>
2019-10-09 15:34:41 +1300

    Removed erroneous imports.

--- src/wai/common/json/serialise/__init__.py
+++ src/wai/common/json/serialise/__init__.py
@@ -3,8 +3,6 @@ Package for interfaces supporting JSON serialisation.
 """
 from ._error import JSONSerialiseError
 from ._JSONBiserialisable import JSONBiserialisable
-from ._JSONDecodable import JSONDecodable
 from ._JSONDeserialisable import JSONDeserialisable
-from ._JSONEncodable import JSONEncodable
 from ._JSONSerialisable import JSONSerialisable
 from ._JSONValidatedBiserialisable import JSONValidatedBiserialisable
