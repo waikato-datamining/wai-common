62e05c2c2a2771ec90a78c84547a5f3dd89cd827
csterling <coreytsterling@gmail.com>
2019-10-11 15:33:43 +1300

    Added missing import.

--- src/wai/common/json/schema/__init__.py
+++ src/wai/common/json/schema/__init__.py
@@ -7,5 +7,5 @@ from ._dynamic import string_schema, standard_object, constant, enum, number, re
 from ._error import JSONSchemaError
 from ._static import TRIVIALLY_SUCCEED_SCHEMA, TRIVIALLY_FAIL_SCHEMA, BOOL_SCHEMA, INT_SCHEMA, FLOAT_SCHEMA, \
     STRING_SCHEMA, POSITIVE_INTEGER_SCHEMA, NEGATIVE_INTEGER_SCHEMA, NON_NEGATIVE_INTEGER_SCHEMA, \
-    NON_POSITIVE_INTEGER_SCHEMA
+    NON_POSITIVE_INTEGER_SCHEMA, NULL_SCHEMA
 from ._typing import JSONSchema, JSONDefinitions, is_schema
