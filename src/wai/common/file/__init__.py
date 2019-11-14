752a898e3bcf1f630ccbf3456080222ea85b05d9
csterling <coreytsterling@gmail.com>
2019-11-15 11:07:27 +1300

    Added public exposure of sugar name for PathContextManager.

--- src/wai/common/file/__init__.py
+++ src/wai/common/file/__init__.py
@@ -1,4 +1,4 @@
 from ._NamedColumnSelection import NamedColumnSelection, SELECTION_TYPE
 from ._functions import load_dir
-from ._PathContextManager import PathContextManager
+from ._PathContextManager import PathContextManager, offset_cwd
 from ._util import ensure_path
