c823ce3a05c44edcce0439cc27f91e8f3e464a5e
csterling <coreytsterling@gmail.com>
2020-03-03 13:03:33 +1300

    Added function to get the import code for a class.

--- src/wai/common/meta/__init__.py
+++ src/wai/common/meta/__init__.py
@@ -10,6 +10,7 @@ from ._functions import (
     does_not_raise,
     all_as_kwargs
 )
+from ._import_code import get_import_code
 from ._instanceoptionalmethod import instanceoptionalmethod
 from ._InstanceProperty import InstanceProperty
 from ._metadata import with_metadata, get_metadata, has_metadata
---
+++ src/wai/common/meta/_import_code.py
@@ -0,0 +1,30 @@
+from typing import Type, Union
+
+
+def get_import_code(cls: Type, indent: Union[int, str] = "") -> str:
+    """
+    Gets the code to use to import a class.
+
+    :param cls:     The class.
+    :param indent:  The indentation of the code.
+    :return:        The code.
+    """
+    # Can't get import code for closure classes
+    if "<locals>" in cls.__qualname__:
+        raise ValueError(f"Can't get import code for closure class '{cls.__qualname__}'")
+
+    # Get the outer-most class to import
+    outer_class = cls.__qualname__[:cls.__qualname__.index(".")] if "." in cls.__qualname__ else cls.__qualname__
+
+    # Format the indentation string
+    if isinstance(indent, int):
+        indent = " " * indent
+
+    # Get the code for importing the outer-most class
+    code = f"{indent}from {cls.__module__} import {outer_class}"
+
+    # If it's an inner class, alias it
+    if cls.__name__ != outer_class:
+        code += f"\n{indent}{cls.__name__} = {cls.__qualname__}"
+
+    return code
