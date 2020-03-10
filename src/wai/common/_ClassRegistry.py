2f2db072802576dffb74d0f93f5df714d8efa96e
csterling <coreytsterling@gmail.com>
2020-03-11 10:34:22 +1300

    Extra functionality for ClassRegistry.

--- src/wai/common/_ClassRegistry.py
+++ src/wai/common/_ClassRegistry.py
@@ -1,4 +1,4 @@
-from typing import Dict, Set, Type, Optional
+from typing import Dict, Set, Type, Optional, Iterator
 
 from .meta import fully_qualified_name
 
@@ -12,13 +12,15 @@ class ClassRegistry:
     def __init__(self):
         self._name_lookup: Dict[str, Set[str]] = {}
         self._qualname_lookup: Dict[str, Type] = {}
-        self._aliases: Dict[str, Type] = {}
+        self._aliases: Dict[str, str] = {}
+        self._alias_reverse_lookup: Dict[str, str] = {}
 
     def register(self, cls: Type):
         """
         Registers a class with the registry.
 
         :param cls:     The class to register.
+        :return:        The fully-qualified name of the class.
         """
         # Get the short and qualified names of the class
         name: str = cls.__name__
@@ -26,7 +28,7 @@ class ClassRegistry:
 
         # Abort if already registered
         if qualname in self._qualname_lookup:
-            return
+            return qualname
 
         # Initialise the short-name list of necessary
         if name not in self._name_lookup:
@@ -38,10 +40,22 @@ class ClassRegistry:
         # Add the type to the qualified lookup
         self._qualname_lookup[qualname] = cls
 
-    def alias(self, cls: Type, alias: str):
+        return qualname
+
+    def is_registered(self, cls: Type) -> bool:
+        """
+        Checks if the given class is registered.
+
+        :param cls:     The class to check.
+        :return:        True if the class is registered,
+                        False if not.
+        """
+        return fully_qualified_name(cls) in self._qualname_lookup
+
+    def alias(self, cls: Type, alias: str) -> 'ClassRegistry':
         """
         Creates an alias for a class. Automatically registers
-        the class if it's not already.
+        the class if it's not already. Chainable.
 
         :param cls:         The class to alias.
         :param alias:       The alias to use.
@@ -55,11 +69,45 @@ class ClassRegistry:
         if alias in self._aliases:
             raise NameError(f"Alias '{alias}' already in use")
 
-        # If it's not already registered, do so now
-        self.register(cls)
+        # Make sure the class is registered
+        qualname = self.register(cls)
 
         # Save the alias
-        self._aliases[alias] = cls
+        self._aliases[alias] = qualname
+        self._alias_reverse_lookup[qualname] = alias
+
+        # Return self so as to be chainable
+        return self
+
+    def resolve(self, name: str) -> Optional[str]:
+        """
+        Attempts to resolve names to their fully-qualified equivalent.
+
+        :param name:        The name to resolve (qualified/short/alias).
+        :return:            The fully-qualified name, or None if not found.
+        :raises NameError:  If the name is an ambiguous short-name.
+        """
+        # If the name is an alias, dereference it
+        if name in self._aliases:
+            return self._aliases[name]
+
+        # If the name is qualified, return the class
+        if name in self._qualname_lookup:
+            return name
+
+        # If the name is a short name, try to resolve it
+        if name in self._name_lookup:
+            # Get the potential qualified names
+            qualnames = self._name_lookup[name]
+
+            # If there's more than one, it's ambiguous
+            if len(qualnames) > 1:
+                raise NameError(f"Class name '{name}' is ambiguous: {', '.join(qualnames)}")
+
+            for qualname in qualnames:  # Dummy loop to get first (only) item from set
+                return qualname
+
+        return None
 
     def find(self, name: str, *,
              attempt_class_import: bool = False) -> Optional[Type]:
@@ -73,25 +121,12 @@ class ClassRegistry:
                                         if not found.
         :raises NameError:              If the name is an ambiguous short-name.
         """
-        # If the name is qualified, return the class
-        if name in self._qualname_lookup:
-            return self._qualname_lookup[name]
+        # Resolve the name to its fully-qualified counterpart
+        qualname = self.resolve(name)
 
-        # If the name is an alias, return the aliased class
-        if name in self._aliases:
-            return self._aliases[name]
-
-        # If the name is a short name, try to resolve it
-        if name in self._name_lookup:
-            # Get the potential qualified names
-            qualnames = self._name_lookup[name]
-
-            # If there's more than one, it's ambiguous
-            if len(qualnames) > 1:
-                raise NameError(f"Class name '{name}' is ambiguous: {', '.join(qualnames)}")
-
-            for qualname in qualnames:  # Dummy loop to get first (only) item from set
-                return self.find(qualname)
+        # If resolution succeeded, return the named class
+        if qualname is not None:
+            return self._qualname_lookup[qualname]
 
         # Attempt to import the class if selected
         if attempt_class_import:
@@ -99,6 +134,35 @@ class ClassRegistry:
 
         # Class not found
         return None
+
+    def aliases(self) -> Iterator[str]:
+        """
+        Gets an iterator over the aliases registered with the registry.
+
+        :return:    The iterator of aliases.
+        """
+        return (str(name) for name in self._aliases.keys())
+
+    def has_alias(self, alias: str) -> bool:
+        """
+        Whether the registry has an alias with the given name.
+
+        :param alias:   The alias to check for.
+        :return:        True if the alias exists, False if not.
+        """
+        return alias in self._aliases
+
+    def get_alias(self, cls: Type) -> Optional[str]:
+        """
+        Gets the alias for a class.
+
+        :param cls:     The class to get the alias for.
+        :return:        The alias, or None if the class is not aliased.
+        """
+        # Get the class' fully-qualified name
+        qualname = fully_qualified_name(cls)
+
+        return self._alias_reverse_lookup.get(qualname, None)
 
     def attempt_class_import(self, name: str) -> Optional[Type]:
         """
